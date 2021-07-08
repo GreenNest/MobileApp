9e7fa72fbae790795409c3a6eaed810b9ae9f6c4
chathurikaPerera <pererachathurika96@gmail.com>
2021-07-09 00:38:41 +0530

    complete welcome page

---
+++ green_nest/lib/screens/home/components/sidebar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xFF386641),
+        padding: EdgeInsets.zero,
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                'chathurika',
+                style: TextStyle(
+                  color: Color(0xFFF2E8CF)
+                ),
+              ),
+              accountEmail: Text(
+                'chathurika@gmail.com',
+                style: TextStyle(
+                    color: Color(0xFFF2E8CF)
+                ),
+              ),
+              currentAccountPicture: CircleAvatar(
+                child: Image.asset(
+                  'assets/image6.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Color(0xFFF2E8CF), size: 35,),
+              title: Text('Home', style: TextStyle(color: Color(0xFFF2E8CF)),),
+              onTap: (){},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
