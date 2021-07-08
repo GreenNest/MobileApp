b4c2a21c4b656e7749a65d41794c54e27aac4ef4
chathurikaPerera <pererachathurika96@gmail.com>
2021-07-09 00:54:51 +0530

    complete welcome

--- green_nest/lib/screens/home/home.dart
+++ green_nest/lib/screens/home/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('hello'),),
+      appBar: AppBar(title: Text('hello world'),),
       drawer: Sidebar(),
 
     );
